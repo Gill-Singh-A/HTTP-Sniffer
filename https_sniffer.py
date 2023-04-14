513883b9451392643d0ad708b0c571eb864f4216
Gill-Singh-A <amansg22@iitk.ac.in>
2023-04-14 21:11:10 +0530

    Updated Python Program

--- https_sniffer.py
+++ https_sniffer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
+from os import geteuid
 from sys import argv
-from scapy.all import sniff, Ether, DHCP, IP, Raw
-from scapy.layers.http import HTTPRequest, HTTPResponse
+from pickle import load, dump
+from scapy.all import sniff, IP, Raw
+from scapy.layers.http import HTTPRequest
 from datetime import date
 from optparse import OptionParser
 from colorama import Fore, Back, Style
-from time import strftime, localtime, time
+from time import strftime, localtime
 
-packets, verbose, store, write = [], True, False, False
+packets_global, verbose = [], True
 
 status_color = {
 	'+': Fore.GREEN,
@@ -17,8 +19,6 @@ status_color = {
 	':': Fore.CYAN,
 	' ': Fore.WHITE,
 }
-
-packets = 0
 
 def get_time():
 	return strftime("%H:%M:%S", localtime())
@@ -31,6 +31,9 @@ def get_arguments(*args):
 		parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
 	return parser.parse_args()[0]
 
+def check_root():
+	return geteuid() == 0
+
 def sniff_packets(iface=None):
 	if iface:
 		sniff(rn=process_packet, iface=iface, store=False)
@@ -38,15 +41,44 @@ def sniff_packets(iface=None):
 		sniff(prn=process_packet, store=False)
 def process_packet(packet):
 	if packet.haslayer(HTTPRequest):
+		packets_global.append(packet)
+		if not verbose:
+			print(f"\rPackets Sniffed = {len(packets_global)}", end='')
 		url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
 		ip = packet[IP].src
 		method = packet[HTTPRequest].Method.decode()
-		display('+', f"{ip} : {Back.MAGENTA}{method}{Back.RESET} => {url}")
+		if verbose:
+			display('+', f"{ip} : {Back.MAGENTA}{method}{Back.RESET} => {url}")
 		if packet.haslayer(Raw):
-			display('*', f"RAW Data : {packet.Raw.load}")
+			if verbose:
+				display('*', f"RAW Data : {packet.Raw.load}")
 
 if __name__ == "__main__":
-	if len(argv) == 2:
-		sniff_packets(argv[1])
-	else:
-		sniff_packets()
+	data = get_arguments(('-i', "--iface", "iface", "Interface on which sniffing has to be done"),
+					     ('-v', "--verbose", "verbose", "Display Useful Information related to the packets on the screen (True/False)(Default = True)"),
+						 ('-w', "--write", "write", "Dump the Packets to file"),
+						 ('-r', "--read", "read", "Read Packets from a dump file"))
+	if data.read:
+		try:
+			with open(data.read, 'rb') as file:
+				packets = load(file)
+		except FileNotFoundError:
+			display('-', f"{Back.MAGENTA}{data.read}{Back.RESET} File not found!")
+			exit(0)
+		except:
+			display('-', f"Error reading from file {Back.MAGENTA}{data.read}{Back.RESET}")
+			exit(0)
+		for packet in packets:
+			process_packet(packet)
+		exit(0)
+	if data.verbose == "False":
+		verbose = False
+	if not check_root():
+		display('-', f"This Program requires {Back.MAGENTA}root{Back.RESET} Privileges")
+		exit(0)
+	sniff_packets(data.iface)
+	print()
+	display(':', f"Total Packets Sniffed = {Back.MAGENTA}{len(packets_global)}{Back.RESET}")
+	if data.write:
+		with open(data.write, 'wb') as file:
+			dump(packets_global, file)
